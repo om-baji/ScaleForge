e7d1e52fcd6304df8f76fba6c5291df1dcccaa72
om-baji <ombaji124@gmail.com>
2025-09-11 02:08:02 +0530

    fix:

--- backend/handlers/global.error.js
+++
@@ -1,8 +0,0 @@
-export class ApiError extends Error {
-    constructor(message, code) {
-        super(message);
-        this.name = 'ApiError';
-        this.code = code;
-        Error.captureStackTrace(this, this.constructor);
-    }
-}
--- backend/routes/user.routes.js
+++
@@ -1,9 +0,0 @@
-import express from "express"
-import { asyncHandler } from "../handlers/async.js";
-import { UserController } from "../controllers/user.controller.js";
-
-const userRouter = express.Router();
-
-userRouter.post("/create", asyncHandler(UserController.create))
-
-export default userRouter
